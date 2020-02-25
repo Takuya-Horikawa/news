818a25e3e9df63d38e33e5e707974a88114294ed
Takuya-Horikawa <takuya.horikawa@kellogg.northwestern.edu>
2020-02-25 07:37:23 +0000

    a

--- app.rb
+++ app.rb
@@ -7,12 +7,24 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "fd60fbab6e9f7187ccd81a0eac203439"
+
 
 get "/" do
   # show a view that asks for the location
+view "ask"
+   
+
+
 end
 
 get "/news" do
+
   # do everything else
+
+
+    view "news"
 end
+
+
+
